fea065a99514d192e96257307e664b4ce2022b13
davier <davier570@gmail.com>
2020-12-11 22:56:35 +0800

    Update 5
    updated main page, added a modal for input log dates. Updated JS and CSS

--- js/main.js
+++ js/main.js
@@ -2,18 +2,79 @@
 //-----------hide signup page on load------------------------------
 $(document).ready(function(){
     $('#signup-page').hide();
-});
+    $('#editDate').hide();
+
 
 //create var to store user details on signup
 
 var username = $('#userName:nth-child(2)');
-var userpass = $('#userPassword');
-var useremail = $('#userEmail');
-var userbday = $('#userDOB');
+var userpass = $('#userPassword:nth-child(2)');
+var useremail = $('#userEmail:nth-child(2)');
+var userbday = $('#userDOB:nth-child(2)');
+
 var logname;
 var logpass;
 var logemail;
 var logbday;
+
+var logStart;
+var logEnd;
+var logStartDay;
+var logStartMonth;
+var logEndDay;
+var logEndMonth;
+
+//------------buttons on click----------------------------------------------
+
+$('#logInBtn').click(function(e){
+    e.preventDefault();
+    login();
+});
+
+$("#signInBtn").click(function(e){
+    e.preventDefault();
+    signup();
+});
+
+$('#loadLog').click(function(e){
+    e.preventDefault();
+    loadLogin();
+});
+
+$('#loadSign').click(function(e){
+    e.preventDefault();
+    loadSignup();
+});
+
+/*$('#setDateBtn').click(function(e){
+    e.preventDefault();
+    logDates();
+    console.log('button clicked');
+});*/
+
+$("body").on("click", "#setDateBtn", function(e) {
+    e.preventDefault();
+    logDates();
+  });
+
+  $("body").on("click", "#clearDateBtn", function(e) {
+    e.preventDefault();
+    logDates();
+  });
+
+
+/*$('#dismiss').click(function(e){
+    dismissModal();
+})
+
+$('#logDateBtn').click(function(e){
+    showLogDate();
+});
+
+$('#closeBtn').click(function(e){
+    closeLogDate();
+});*/
+
 
 
 /*document.getElementById("signupBtn").addEventListener("click", function(event){
@@ -30,10 +91,10 @@ document.getElementById("signupBtn").addEventListener("click", function(event){
 function signup(){
     //event.preventDefault;
 
-    logname = username.value;
-    logpass = userpass.value;
-    logemail = useremail.value;
-    logbday = userbday.value;
+    logname = username.val();
+    logpass = userpass.val();
+    logemail = useremail.val();
+    logbday = userbday.val();
 
     localStorage.setItem('storedName', logname);
     localStorage.setItem('storedPass', logpass);
@@ -41,30 +102,32 @@ function signup(){
     localStorage.setItem('storedBday', logbday);
 
     //var test1 = localStorage.getItem("storedName");
-
+    logbday = logbday.slice(5, 11);
     //alert(test1);
 
-    console.log("Sign up success! " + logname + " " + logpass + " " + logemail + " " + logbday);
+    console.log("Sign up success! " + logname + " " + logemail + " " + logpass + " " + logbday);
 }
 
 
 //-----------------log in function---------------------------------
 
-function login(event){
-    event.preventDefault;
+function login(){
+    //e.preventDefault();
     var storedEmail = localStorage.getItem('storedEmail');
     var storedPass = localStorage.getItem('storedPass');
 
-    var enteredEmail = $('#logEmail:nth-child(0)');
-    var enteredPass =  $('#logPassword:nth-child(0)');
+    var enteredEmail = $('#logEmail:nth-child(2)');
+    var enteredPass =  $('#logPassword:nth-child(2)');
 
-    if ((enteredEmail.value == storedEmail) && (enteredPass.value == storedPass)){
+    if ((enteredEmail.val() == storedEmail) && (enteredPass.val() == storedPass)){
         console.log("logged in!")
         alert("successfully logged in!");
-        window.location("./main.html");
+
+        window.location.href = "./main.html";
     }
     else{
         console.log("wrong email/pass");
+        alert("wrong email/password");  
     }
 }
 
@@ -88,20 +151,73 @@ function loadLogin(){
 }
 
 /*--------------------select date page---------------------------------------*/
+
+function logDates(){
+    var enteredStart = $('#userStart:nth-child(2)');
+    var enteredEnd = $('#userEnd:nth-child(2)');
+    logStart = enteredStart.val();
+    logEnd = enteredEnd.val();
+
+    logStartMonth = logStart.slice(5, 7);
+    logStartDay = logStart.slice(8, 10);
+
+    console.log("start date " + logStartDay + " " + logStartMonth);
+
+    logEndMonth = logEnd.slice(5, 7);
+    logEndDay = logEnd.slice(8, 10);
+
+    console.log("end date " + logEndDay + " " + logEndMonth);
+
+    localStorage.setItem('storedStartDay', logStartDay);
+    localStorage.setItem('storedStartMonth', logStartMonth);
+
+    localStorage.setItem('storedEndDay', logEndDay);
+    localStorage.setItem('storedEndMonth', logEndMonth);
+  
+  //console.log("success! " + logStart + " " + logEnd)
+
+}
+
+
+
+});
+
+//-----------------END OF DOCUMENT ON READY-----------------------------------------------
+
 customElements.define('modal-content', class ModalContent extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
-        <ion-header translucent>
+
+        <ion-header>
           <ion-toolbar>
             <ion-title>Log your dates!</ion-title>
             <ion-buttons slot="end">
-              <ion-button onclick="dismissModal()">Close</ion-button>
+              <ion-button id="dismiss" onclick="dismissModal()"><ion-icon name="close" size="large"></ion-icon></ion-button>
             </ion-buttons>
           </ion-toolbar>
         </ion-header>
-        <ion-content fullscreen>
 
-          put your calendar here
+        <br><br>
+
+        <ion-content>
+          <ion-row>
+          <ion-col class="setDates">
+          <form id="log-date-form">
+          <ion-item lines="full">
+            <ion-label>Period Start date</ion-label>
+            <ion-datetime picker-format="D M" display-format="D MMM" placeholder="Select Date" min="2020" id="userStart" required></ion-datetime>
+          </ion-item>
+        <br>
+          <ion-item lines="full">
+            <ion-label>Period End date</ion-label>
+            <ion-datetime picker-format="D M" display-format="D MMM" placeholder="Select Date" min="2020" id="userEnd" required></ion-datetime>
+          </ion-item>
+
+          <ion-button type="button" color="warning" expand="block" id="setDateBtn">Set</ion-button>
+          
+          <form>
+          </ion-col>
+          </ion-row>
           
         </ion-content>
       `;
@@ -109,6 +225,7 @@ customElements.define('modal-content', class ModalContent extends HTMLElement {
   });
 
   let currentModal = null;
+
   const button = document.getElementById('logDateBtn');
   if(button){
     button.addEventListener('click', createModal);
@@ -116,11 +233,19 @@ customElements.define('modal-content', class ModalContent extends HTMLElement {
   else{
       console.log("button is null/undefined");
   }
+
+  const button2 = document.getElementById('dismiss');
+  if(button2){
+      button2.addEventListener('click', dismissModal);
+  }
+  else{
+  }
   
 
   async function createModal() {
     const modal = await modalController.create({
-      component: 'modal-content'
+      component: 'modal-content',
+      cssClass: 'customModal'
     });
 
     await modal.present();
@@ -132,3 +257,5 @@ customElements.define('modal-content', class ModalContent extends HTMLElement {
       currentModal.dismiss().then(() => { currentModal = null; });
     }
   }
+
+  
